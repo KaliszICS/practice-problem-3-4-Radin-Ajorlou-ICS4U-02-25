bec828699bcb530b37f742872175e940546c5c67
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-04-09 23:06:36 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		System.out.print(Arrays.toString(recaman(-5)));
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+
+
+
+
+	public static int[] recaman (int n){
+		ArrayList<Integer> arr = new ArrayList<>();
+		recamanHelp(n, arr);
+		if (n<=0){
+			return new int[0];
+		}
+		int[] array = new int[n];
+		for (int i = 0; i < n; i++){
+			array[i] = arr.get(i);
+		}
+		return array;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+	public static int recamanHelp(int n, ArrayList<Integer> arr){
+		if (n <= 0){
+			return 0;
+		}
+		int num = recamanHelp(n - 1, arr);
+		int possibleNum = num - n;
+		int thisNum = num + n;
+		if (possibleNum > 0){
+			for (int i = 0; i < arr.size(); i++){
+				if (possibleNum == arr.get(i)){
+					arr.add(thisNum);
+					return thisNum;
+				}
+			}
+			arr.add(possibleNum);
+			return possibleNum;
+		}
+		else {
+			arr.add(thisNum);
+			return thisNum;
+		}
 
+	}
 }
